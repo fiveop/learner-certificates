a7a14967f5be85ff69ded665cb6f00101409027c
Philipp Matthias Schäfer <philipp.matthias.schaefer@posteo.de>
2024-11-28 09:55:10 +0100

    Remove trailing whitespace

--- bin/makecert.sh
+++ bin/makecert.sh
@@ -6,7 +6,7 @@ if [ $# -le 2 ]; then
     exit 1;
 fi
 
-# Check for jinja2 
+# Check for jinja2
 command -v jinja2 >/dev/null 2>&1 || { echo >&2 "This scripts requires jinja2-cli but it's not installed. Install with \`pip install jinja2-cli\`.  Aborting."; exit 1; }
 # Check for svg2pdf
 command -v svg2pdf>/dev/null 2>&1 || { echo >&2 "This script requires svg2pdf, a part of svglib. Install with \`pip install svglib\`. Aborting."; exit 1; }
