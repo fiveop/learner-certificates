ed0944be0e4cce1b039f1478a0e6aff6ab0df2cd
Philipp Matthias Schäfer <philipp.matthias.schaefer@posteo.de>
2024-11-28 09:52:41 +0100

    Change argument handling
    - check for minimum amount of arguments
    - exit if this check fails
    - allow for multiple arguments passed to jinja2
    - quote filenames to allow for spaces in filenames

--- bin/makecert.sh
+++ bin/makecert.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [[ $# -eq 0 ]]; 
-then
-	echo "makecert.sh <source.svg> <context> <outputfile.pdf>"
+
+if [ $# -le 2 ]; then
+    echo "makecert.sh <source.svg> <outputfile.pdf> <jinja2 args>..."
+    exit 1;
 fi
 
 # Check for jinja2 
@@ -10,13 +11,14 @@ command -v jinja2 >/dev/null 2>&1 || { echo >&2 "This scripts requires jinja2-cl
 # Check for svg2pdf
 command -v svg2pdf>/dev/null 2>&1 || { echo >&2 "This script requires svg2pdf, a part of svglib. Install with \`pip install svglib\`. Aborting."; exit 1; }
 
-source_svg=${1}
-context=${2} 
-certout_pdf=${3}
+source_svg="$1"
+
+certout_pdf="$2"
 certout_svg=${certout_pdf/.pdf/.svg}
 
-jinja2 ${source_svg} ${context} > ${certout_svg} # fill template
-
-svg2pdf ${certout_svg} # convert to PDF
+shift 2
 
 
+jinja2 "${source_svg}" "$@" > "${certout_svg}" # fill template
+
+svg2pdf "${certout_svg}" # convert to PDF
